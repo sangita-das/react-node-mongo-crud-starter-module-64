be77faa4c4786327410c1d8549251120cc6459c2
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-10-24 16:52:44 +0600

    added all

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, []);
+
+    // DELETE AN USER
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure, you want to delete?')
+        if (proceed) {
+            const url = `http://localhost:5000/users/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingUsers = users.filter(user => user.id !== id);
+                        setUsers(remainingUsers);
+                    }
+                });
+
+        }
+    }
+
     return (
         <div>
-            <h2>This is Users</h2>
-        </div>
+            <h2>Users Available: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                        key={user._id}
+                    >{user.name} :: {user.email}
+                        <Link to={`/users/update/${user._id}`}><button>Update</button></Link>
+                        <button onClick={() => handleDeleteUser(user._id)}>x</button>
+                    </li>)
+
+                }
+            </ul>
+        </div >
     );
 };
 
